1ba88e012d55dd6b238efbe22e234aeba4745bd1
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-20 10:46:49 +0900

    feat: add jwt parse method

--- src/main/kotlin/dev/chanlog/chanlogserver/global/security/jwt/JwtProvider.kt
+++ src/main/kotlin/dev/chanlog/chanlogserver/global/security/jwt/JwtProvider.kt
@@ -1,5 +1,9 @@
 package dev.chanlog.chanlogserver.global.security.jwt
 
+import dev.chanlog.chanlogserver.global.security.exception.BasicException
+import dev.chanlog.chanlogserver.global.security.exception.ErrorCode
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.stereotype.Component
@@ -9,7 +13,21 @@ import kotlin.collections.HashMap
 @Component
 class JwtProvider {
 
-  fun createToken(type: TokenType, payload: Map<String, Any>) =
+  fun parseToken(type: TokenType, token: String): Claims {
+        return try {
+            Jwts.parserBuilder()
+                .setSigningKey(type.getSecret())
+                .build()
+                .parseClaimsJws(token)
+                .body
+        } catch (e: ExpiredJwtException) {
+            throw BasicException(ErrorCode.EXPIRED_TOKEN)
+        } catch (e: Exception) {
+            throw BasicException(ErrorCode.INVALID_TOKEN)
+        }
+    }
+
+  fun createToken(type: TokenType, payload: Map<String, Any>): String =
     Jwts.builder()
       .setHeader(createJwtHeader(type))
       .signWith(type.getSecret(), SignatureAlgorithm.HS256)
