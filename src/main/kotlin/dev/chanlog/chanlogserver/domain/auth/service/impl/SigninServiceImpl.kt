501e3990af2aa3730e55e0f51f86a35cc72d1c5a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-23 15:32:46 +0900

    refactor: move business logic to method

--- src/main/kotlin/dev/chanlog/chanlogserver/domain/auth/service/impl/SigninServiceImpl.kt
+++ src/main/kotlin/dev/chanlog/chanlogserver/domain/auth/service/impl/SigninServiceImpl.kt
@@ -1,5 +1,6 @@
 package dev.chanlog.chanlogserver.domain.auth.service.impl
 
+import dev.chanlog.chanlogserver.domain.auth.dto.dto.CreateJwtTokenDto
 import dev.chanlog.chanlogserver.domain.auth.dto.request.SigninRequestDto
 import dev.chanlog.chanlogserver.domain.auth.dto.response.SigninResponseDto
 import dev.chanlog.chanlogserver.domain.auth.entity.Refresh
@@ -29,24 +30,34 @@ class SigninServiceImpl(
   private lateinit var environment: String
 
   override fun execute(data: SigninRequestDto): SigninResponseDto {
+    userCheck(data)
+
+    val tokens = createJwtToken(data)
+
+    val accessToken = createCookie("accessToken", tokens.accessJwt)
+    val refreshToken = createCookie("refreshToken", tokens.refreshJwt)
+
+    return SigninResponseDto(accessToken, refreshToken)
+  }
+
+  private fun userCheck(data: SigninRequestDto) {
     val user = userRepository.findById(data.id).orElse(null) ?: throw BasicException(ErrorCode.FAILED_TOKEN)
 
     val matches = passwordEncoder.matches(data.password, user.password)
     if (!matches) throw BasicException(ErrorCode.FAILED_TOKEN)
+  }
 
+  private fun createJwtToken(data: SigninRequestDto): CreateJwtTokenDto {
     val payload = jwtProvider.createPayload(data.id)
     val accessJwt = jwtProvider.createToken(TokenType.ACCESS, payload)
     val refreshJwt = jwtProvider.createToken(TokenType.REFRESH, payload)
 
     refreshRepository.save(Refresh(refreshJwt.token))
 
-    val accessToken = createCookie("accessToken", accessJwt)
-    val refreshToken = createCookie("refreshToken", refreshJwt)
-
-    return SigninResponseDto(accessToken, refreshToken)
+    return CreateJwtTokenDto(accessJwt, refreshJwt)
   }
 
-  fun createCookie(name: String, token: Token): Cookie {
+  private fun createCookie(name: String, token: Token): Cookie {
     val cookie = Cookie(name, token.token)
     cookie.isHttpOnly = true
     cookie.maxAge = token.expired.time.toInt()
