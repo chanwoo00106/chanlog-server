b2b315f906ead05ba77c7b6729742ebb0a6cec43
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-29 10:01:53 +0900

    feat: add ExceptionFilter

--- src/main/kotlin/dev/chanlog/chanlogserver/global/exception/ErrorCode.kt
+++ src/main/kotlin/dev/chanlog/chanlogserver/global/exception/ErrorCode.kt
@@ -1,8 +1,9 @@
 package dev.chanlog.chanlogserver.global.exception
 
 enum class ErrorCode (val status: Int, val message: String) {
+  FAILED_TOKEN(401, "로그인에 실패했습니다"),
   EXPIRED_TOKEN(403, "토큰 값이 만료되었습니다"),
   INVALID_TOKEN(403, "유효하지 않은 토큰"),
-  FAILED_TOKEN(401, "로그인에 실패했습니다");
 
+  INTERNAL_ERROR(500, "서버 내부에 문제가 생겼습니다.\n잠시 후에 다시 이용해 주세요");
 }
