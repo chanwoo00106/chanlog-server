235f8e32000e802f647f1dac7e8e609f4bf60573
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-03 08:25:00 +0900

    feat: add securityConfig

---
+++ src/main/kotlin/dev/chanlogserver/global/security/SecurityConfig.kt
@@ -0,0 +1,46 @@
+package dev.chanlogserver.global.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+  @Bean
+  fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    http
+      .csrf().disable()
+      .httpBasic().disable()
+      .formLogin().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+    http.authorizeHttpRequests()
+      .requestMatchers(HttpMethod.GET, "/test").permitAll()
+
+      .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+      .requestMatchers(HttpMethod.PATCH, "/auth").authenticated()
+      .requestMatchers(HttpMethod.DELETE, "/auth").authenticated()
+
+      .requestMatchers(HttpMethod.GET, "/blog").permitAll()
+      .requestMatchers(HttpMethod.POST, "/blog").hasRole("ADMIN")
+      .requestMatchers(HttpMethod.PUT, "/blog").hasRole("ADMIN")
+      .requestMatchers(HttpMethod.DELETE, "/blog").hasRole("ADMIN")
+
+      .requestMatchers(HttpMethod.POST, "/image").hasRole("ADMIN")
+
+      .anyRequest().denyAll()
+
+    return http.build()
+  }
+
+  @Bean
+  fun passwordEncoder(): PasswordEncoder
+    = BCryptPasswordEncoder()
+}
