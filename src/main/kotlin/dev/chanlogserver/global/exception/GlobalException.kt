9e51ba7a2fb41c51987d88d394aac9e32043a8d7
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-03 14:08:24 +0900

    feat: add validation exception

---
+++ src/main/kotlin/dev/chanlogserver/global/exception/GlobalException.kt
@@ -0,0 +1,20 @@
+package dev.chanlogserver.global.exception
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalException {
+  @ExceptionHandler(MethodArgumentNotValidException::class)
+  fun validationException(
+    request: HttpServletRequest,
+    exception: MethodArgumentNotValidException
+  ): ResponseEntity<ExceptionResponse> {
+    val responseBody = ExceptionResponse(HttpStatus.BAD_REQUEST.value(), request.requestURI, exception.bindingResult.allErrors[0].defaultMessage)
+    return ResponseEntity(responseBody, HttpStatus.BAD_REQUEST)
+  }
+}
