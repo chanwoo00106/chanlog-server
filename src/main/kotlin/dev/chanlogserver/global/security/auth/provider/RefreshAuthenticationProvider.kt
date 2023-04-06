f0ed1bf9185099aaef9682eb82881373fb4c7bc8
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-06 13:50:42 +0900

    feat: Add UserDetailsService

--- src/main/kotlin/dev/chanlogserver/global/security/auth/provider/RefreshAuthenticationProvider.kt
+++ src/main/kotlin/dev/chanlogserver/global/security/auth/provider/RefreshAuthenticationProvider.kt
@@ -6,14 +6,21 @@ import dev.chanlogserver.global.security.auth.authentication.RefreshAuthenticati
 import dev.chanlogserver.global.security.jwt.JwtProvider
 import dev.chanlogserver.global.security.jwt.TokenType
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetailsService
 
 class RefreshAuthenticationProvider(
-  private val jwtProvider: JwtProvider
+  private val jwtProvider: JwtProvider,
+  private val userDetailsService: UserDetailsService
 ): AuthenticationProvider {
   override fun authenticate(authentication: Authentication): Authentication {
     val data = jwtProvider.parseToken(TokenType.REFRESH, authentication.name)
     if (data["id"] == null) throw BasicException(ErrorCode.INVALID_TOKEN)
+
+    val userDetails = userDetailsService.loadUserByUsername(data["id"].toString())
+
+    return UsernamePasswordAuthenticationToken(userDetails.username, userDetails.password, userDetails.authorities)
   }
 
   override fun supports(authentication: Class<*>) = RefreshAuthentication::class == authentication::class
