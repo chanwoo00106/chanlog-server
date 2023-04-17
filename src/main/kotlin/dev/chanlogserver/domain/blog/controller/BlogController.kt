bf41429d36261f7d011fa9e3301c3f0df0877c03
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-18 00:26:18 +0900

    feat: add controller, dto

--- src/main/kotlin/dev/chanlogserver/domain/blog/controller/BlogController.kt
+++ src/main/kotlin/dev/chanlogserver/domain/blog/controller/BlogController.kt
@@ -3,15 +3,19 @@ package dev.chanlogserver.domain.blog.controller
 import dev.chanlogserver.domain.blog.Blog
 import dev.chanlogserver.domain.blog.dto.request.CreateRequestDto
 import dev.chanlogserver.domain.blog.dto.request.FindBlogRequestDto
+import dev.chanlogserver.domain.blog.dto.request.ModifyBlogRequestDto
 import dev.chanlogserver.domain.blog.dto.response.FindBlogResponseDto
 import dev.chanlogserver.domain.blog.service.CreateBlogService
 import dev.chanlogserver.domain.blog.service.FindBlogService
 import jakarta.validation.Valid
+import jakarta.validation.constraints.NotEmpty
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.Authentication
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/blog")
+@Validated
 class BlogController(
   private val createBlogService: CreateBlogService,
   private val findBlogService: FindBlogService
@@ -37,4 +42,10 @@ class BlogController(
   ): List<FindBlogResponseDto> {
     return findBlogService.execute(FindBlogRequestDto(title, page, email))
   }
+
+  @PutMapping
+  fun modifyBlog(
+    @RequestParam("title") @NotEmpty(message = "title은 필수값입니다") title: String,
+    @RequestBody @Valid body: ModifyBlogRequestDto
+  ) {}
 }
